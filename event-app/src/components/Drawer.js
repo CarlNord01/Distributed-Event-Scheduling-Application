acd54990f31483b4658a1880d94952eb0fa20663
CarlNord01 <165204083+CarlNord01@users.noreply.github.com>
2025-03-02 15:46:27 +0100

    Update Drawer.js

--- event-app/src/components/Drawer.js
+++ event-app/src/components/Drawer.js
@@ -22,7 +22,7 @@ export default function ProfileDrawer({ open, onClose, handleLogout }) {
   // Fetch the logged-in user's information when the component mounts
   useEffect(() => {
     axios
-      .get('http://localhost:5001/api/session', { withCredentials: true })
+      .get('http://9.223.200.206:5001/api/session', { withCredentials: true })
       .then((response) => {
         if (response.data.user) {
           setUserId(response.data.user.userId); // Set the userId state

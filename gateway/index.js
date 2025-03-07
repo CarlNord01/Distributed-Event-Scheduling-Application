9fce3a1c31e914c6019f10ab01c8fc97110beafb
CarlNord01 <165204083+CarlNord01@users.noreply.github.com>
2025-03-07 17:11:00 +0100

    testing new proxy

--- gateway/index.js
+++ gateway/index.js
@@ -18,27 +18,19 @@ const JWT_SECRET = process.env.JWT_SECRET || 'very-secret-haha';
 
 // Proxy Middleware with JWT verification
 function createDynamicProxy(targetIP) {
-    return createProxyMiddleware({
-        target: `http://${targetIP}`,
-        changeOrigin: true,
-        onProxyReq: (proxyReq, req, res) => {
-          if (req.body) {
-            const bodyData = JSON.stringify(req.body);
-            proxyReq.setHeader('Content-Type', 'application/json');
-            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-            proxyReq.write(bodyData);
-          }
-
-            // Forward JWT in Authorization header
-            if (req.headers.authorization) {
-                proxyReq.setHeader('Authorization', req.headers.authorization);
-            }
-        },
-        onError: (err, req, res) => {
-          console.error("Proxy Error:", err);
-          res.status(500).send({ error: "Proxy error occurred" });
-        }
-    });
+  return createProxyMiddleware({
+    target: `http://${targetIP}`,
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+      if (req.headers.authorization) {
+        proxyReq.setHeader('Authorization', req.headers.authorization);
+      }
+    },
+    onError: (err, req, res) => {
+      console.error("Proxy Error:", err);
+      res.status(500).send({ error: "Proxy error occurred" });
+    }
+  });
 }
 
 app.use((req,res,next)=>{
